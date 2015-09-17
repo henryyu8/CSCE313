046f70671f4e098f00e0f8a829147a364efe8b58
Henry <henryyu8@tamu.edu>
2015-09-16 20:18:17 -0500

    Assignment1

---
+++ Assignment1/MP1_sources/src/part2/linked_list2.c
@@ -0,0 +1,9 @@
+#include "linked_list2.h"
+
+void 	Init (int M, int b, int t){} 
+void 	Destroy (){} 		 
+int 	Insert (int key,char * value_ptr, int value_len){}
+int 	Delete (int key){}
+char* 	Lookup (int key){}
+void 	PrintList (){}
+
