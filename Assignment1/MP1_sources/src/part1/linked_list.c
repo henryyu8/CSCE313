1a857d144ca62369b911c36da3ca3e55b5ba369c
Xiangdong Yu <henryyu8@users.noreply.github.com>
2015-09-17 13:05:45 -0500

    Update linked_list.c

--- Assignment1/MP1_sources/src/part1/linked_list.c
+++ Assignment1/MP1_sources/src/part1/linked_list.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 #include "linked_list.h"
 
-void Init (int M, int b){
-	sdfsad
-	sdnfosdbvoshj
+void LinkedList::Init (int M, int b2){
+	mem_allocation = malloc(M*sizeof(char));
+	b = b2;
+	fre_ptr = mem_allocation;
 }
 
-void Destroy (){
+void LinkedList::Destroy (){
 	
+	free(mem_allocation);
 }
  		 
-int Insert (int key,char * value_ptr, int value_len){
+int LinkedList::Insert (int key,char * value_ptr, int value_len){
+	Node *n = fre_ptr;
+	n->key = key;
+	n->val_len = value_len;
+	memcpy(n+1, value_ptr, val_len)
+	//LinkedList *item = new LinkedList();
+	fre_ptr += b;
+	n -> next = NULL;
 	
 }
 
-int Delete (int key){
-	
+int LinkedList::Delete (int key){
+	Node *n = new Node();
+	delete node->key;
 }
 
-char* Lookup (int key){
+char* LinkedList::Lookup (int key){
+	Node *n = new Node();
+	
 	return NULL;
 }
 
-void PrintList (){
-	
+void LinkedList::PrintList (){
+	Node *n = new Node
+	cout
 }
