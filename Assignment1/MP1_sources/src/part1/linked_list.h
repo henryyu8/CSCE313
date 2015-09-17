20a4e99e600ef5a54d59e9d3cc8a71c6a282dd97
Xiangdong Yu <henryyu8@users.noreply.github.com>
2015-09-17 13:06:31 -0500

    Update linked_list.h

--- Assignment1/MP1_sources/src/part1/linked_list.h
+++ Assignment1/MP1_sources/src/part1/linked_list.h
@@ -1,23 +1,38 @@
+#include <cstdlib>
+#include <iostream>
+
 #ifndef _linked_list_h_                   // include file only once
 #define _linked_list_h_
 
+using namespace std;
+struct LinkedList;
+
 struct node {
-	int x;
-	struct node *next;
-};
+	node *next;
+	int key;
+	int val_len;
+	//char* value[b-16];
+	} node1;
 
 	//root = malloc(sizeof(struct node));
 
-struct 
-	char *buffer = new char[n];
-	void 	Init (int M, int b); // initializes the linked list, should be called once from the main
-	void 	Destroy (); 		 //	destroys the linked list and cleans resources
-	int 	Insert (int key,char * value_ptr, int value_len); // inserts the key and copies the value to the payload
-	int 	Delete (int key); 	 // delete the whole block containing that particular key. When multiple entries with the same key, delete only the first one	
-	char* 	Lookup (int key);	 //	Looks up the first item with the given and returns a pointer to the value portion (the value length and the actual value) 
+struct LinkedList {
+	private:
+		int M;
+		int b;
+		char *fre_ptr;
+		char *mem_allocation;
+	public:
+		//char *buffer = new char[M];
+		void 	Init (int M, int b); // initializes the linked list, should be called once from the main
+		void 	Destroy (); 		 //	destroys the linked list and cleans resources
+		int 	Insert (int key,char * value_ptr, int value_len); // inserts the key and copies the value to the payload
+		int 	Delete (int key); 	 // delete the whole block containing that particular key. When multiple entries with the same key, delete only the first one	
+		char* 	Lookup (int key);	 //	Looks up the first item with the given and returns a pointer to the value portion (the value length and the actual value) 
 							//(the user can read or modify after obtaining the pointer)
-	void 	PrintList ();		// prints the entire list by following the next pointers. Print the keys and the length	of the value
-}
+		void 	PrintList ();		// prints the entire list by following the next pointers. Print the keys and the length	of the value
+	
+	};
 
 
 #endif 
